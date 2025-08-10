d01bde3aef9cc4599ef29d3aad8484f0318f412c
lev <71851874+pastedaf@users.noreply.github.com>
2025-08-10 19:18:00 +1200

    fix: add guard in fast_io’s wincrt.h so ucrt_iobuf is only used when _MSC_VER or _UCRT is defined; otherwise it falls back to FILE*.

--- include/fast_io_legacy_impl/c/wincrt.h
+++ include/fast_io_legacy_impl/c/wincrt.h
@@ -1,4 +1,4 @@
-﻿#pragma once
+#pragma once
 
 #if defined(_MSC_VER) && !defined(__clang__)
 #pragma warning(push)
@@ -423,7 +423,11 @@ inline void wincrt_set_buffer_curr_ptr_impl(FILE *__restrict fpp,
 #endif
 											void *ptr) noexcept
 {
+#if defined(_MSC_VER) || defined(_UCRT)
 	ucrt_iobuf *fp{reinterpret_cast<ucrt_iobuf *>(fpp)};
+#else
+	FILE* fp{fpp};
+#endif
 	fp->_cnt -= static_cast<::std::int_least32_t>(
 		static_cast<::std::uint_least32_t>(static_cast<::std::size_t>(reinterpret_cast<char *>(ptr) - fp->_ptr)));
 	fp->_ptr = reinterpret_cast<char *>(ptr);
